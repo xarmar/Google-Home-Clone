518fb29caf8943ef3cf1fd09a3e02ab103600dd4
Xavier Marques <xavierpmarques@gmail.com>
2021-04-14 13:59:32 +0100

    add javascript to fix images that fail to load

---
+++ Javascript/javascript.js
@@ -0,0 +1,15 @@
+var readyState = document;
+
+if (readyState = "complete") {
+
+        function changeUserImage () {
+            image = document.getElementById("user-initial");
+            image.src = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2Fe-GwktHmR3s%2Fhqdefault.jpg&f=1&nofb=1";
+        }
+        
+        function changeGoogleImage () {
+            image = document.getElementById("google-img");
+            image.src = "https://www.google.pt/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
+        }
+ 
+    }
